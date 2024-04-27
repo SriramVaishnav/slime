30e1fda5a9dd1ef31fb4bc3fd3ac1a2a46b8ee38
Sriram Vaishnav R <sriramvaishnavr@gmail.com>
2024-04-27 11:20:36 +0530

    project & files setup

---
+++ app/(tabs)/profile.jsx
@@ -0,0 +1,14 @@
+import { Text, View } from 'react-native'
+import React, { Component } from 'react'
+
+export class profile extends Component {
+  render() {
+    return (
+      <View>
+        <Text>profile</Text>
+      </View>
+    )
+  }
+}
+
+export default profile
