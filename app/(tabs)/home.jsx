0ed2c672660513e8a384dabc395e08ddbca907c7
Sriram Vaishnav R <sriramvaishnavr@gmail.com>
2024-04-29 22:10:29 +0530

    home screen implemented

--- app/(tabs)/home.jsx
+++ app/(tabs)/home.jsx
@@ -6,13 +6,14 @@ import { images } from '../../constants';
 import SearchInput from '../../components/SearchInput';
 import Trending from '../../components/Trending';
 import EmptyState from '../../components/EmptyState';
-import { getAllPosts } from '../../lib/appwrite';
+import { getAllPosts, getLatestPosts } from '../../lib/appwrite';
 import useAppwrite from '../../lib/useAppwrite';
 import VideoCard from '../../components/VideoCard';
 
 const Home = () => {
 
   const { data: posts, refetch } = useAppwrite(getAllPosts);
+  const { data: latestPosts } = useAppwrite(getLatestPosts);
 
   const [refresing, setRefresing] = useState(false);
 
@@ -59,7 +60,7 @@ const Home = () => {
                 Latest Videos
               </Text>
 
-              <Trending posts={[{id: 1}, {id: 2}, {id: 3}] ?? []} />
+              <Trending posts={latestPosts ?? []} />
             </View>
           </View>
         )}
--- components/VideoCard.jsx
+++ components/VideoCard.jsx
@@ -1,6 +1,7 @@
 import { View, Text, Image, TouchableOpacity } from "react-native";
 import React, { useState } from "react";
 import { icons } from "../constants";
+import { ResizeMode, Video } from "expo-av";
 
 const VideoCard = ({
   video: {
@@ -46,7 +47,18 @@ const VideoCard = ({
       </View>
 
       {play ? (
-        <Text>Playing</Text>
+        <Video
+        source={{ uri: video }} 
+        className="w-full h-60 rounded-xl"
+        resizeMode={ResizeMode.CONTAIN}
+        useNativeControls
+        shouldPlay
+        onPlaybackStatusUpdate={(status) => {
+          if(status.didJustFinish){
+            setPlay(false)
+          }
+        }}
+      />
       ) : (
         <TouchableOpacity
           activeOpacity={0.7}
